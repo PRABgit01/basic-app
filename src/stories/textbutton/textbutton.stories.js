a385a05828b0e6478bf7b4a3ae425f7f7774934e
prab <prabhanjan.sharma@spreadd.io>
2023-12-14 00:09:44 +0530

    done all the buttons

---
+++ src/stories/textbutton/textbutton.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Textbutton from "./textbutton";
+
+export default {
+    title:"text button",
+    component:Textbutton
+};
+
+export const textbuttons=()=><Textbutton />
